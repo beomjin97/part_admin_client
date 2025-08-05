e7fa34a7d74051a78deb19fe92be392f0e7c4661
Beomjin <qjawls646@gmail.com>
2025-08-05 16:16:00 +0900

    로그인 form component 작성

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,9 +1,15 @@
+'use client'
+
+import { useActionState } from "react";
 import DefaultForm from "./DefaultForm";
 import DefaultInput from "./DefaultInput";
+import { signin } from "@/utils/action";
 
 export default function LoginForm() {
+    const [state, formAction, isPending] = useActionState(signin, {message: ''})
+
     return (
-        <DefaultForm  title="로그인" submitText="로그인">
+        <DefaultForm  title="로그인" submitText="로그인" formAction={formAction} isPending={isPending}>
           <DefaultInput label="아이디" name="user_id" />
           <DefaultInput label="패스워드" name="password" />
         </DefaultForm>
