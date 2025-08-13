fad11251082fa2b8d009e76b41495ecd1bd9d9ff
Beomjin <qjawls646@gmail.com>
2025-08-13 15:46:31 +0900

    히스토리 생성 form 구현

--- src/app/history/[id]/page.tsx
+++ src/app/history/[id]/page.tsx
@@ -1,17 +1,23 @@
+'use client'
+
 import DefaultForm from "@/components/DefaultForm"
 import DefaultInput from "@/components/DefaultInput"
+import { createHistory } from "@/utils/action"
 import { axiosInstance } from "@/utils/axiosInstance"
+import { useActionState } from "react"
 
 export default async function History({params}: {params: Promise<{id: string}>}) {
     
     const { id } = await params
-
     const { data } = await axiosInstance.get(`part/${id}`);
-    console.debug(data)
+
+    const [state, formAction, isPending] = useActionState(createHistory, {message: id})
+    
+    
     return (
         <>
             <div>history/{id}</div>
-            <DefaultForm title="기록 추가" submitText="추가">
+            <DefaultForm title="기록 추가" submitText="추가" formAction={formAction} isPending={isPending}>
                 <DefaultInput label="수량" name="count" type="number"/>
                 <DefaultInput label="반출" name="is_import" type="radio" value={true}/>
                 <DefaultInput label="반입" name="is_import" type="radio" value={false}/>
